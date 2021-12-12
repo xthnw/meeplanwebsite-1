9c4bb0cc17c80f45d0a7f276eb1a8f47289b4cae
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2021-12-12 16:26:41 +0000

    change about us image

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -2,7 +2,7 @@
 import Logobar from '../Picture/Logobar.png'
 import web from '../Picture/web.png'
 import Wio from '../Picture/Wio.png'
-import About from '../Picture/About.png'
+import About from '../Picture/abu2.jpg'
 import "/src/App.css"
 
 
